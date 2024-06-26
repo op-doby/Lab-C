4a29d65b6b8d72a2f4d4b14055ab6a6042ac81a7
“op-doby” <ophir7@gmail.com>
2024-06-26 20:29:16 +0300

    finished part 1, 2

--- Lab_C/mypipeline.c
+++ Lab_C/mypipeline.c
@@ -5,68 +5,53 @@
 #include <errno.h>
 
 int main() {
-    int pipe_fd[2]; // Array to hold the read and write file descriptors for the pipe
-    pid_t child1_pid, child2_pid;
-
-    if (pipe(pipe_fd) == -1) { // Create a pipe
+    int pipeFd[2]; // Array to hold the read and write file descriptors for the pipe - pipeFd[0] read, pipeFd[1] write
+    pid_t c1pid, c2pid; // Process IDs for the child processes
+    if (pipe(pipeFd) == -1) { // Create the pipe and store the file descriptors in pipeFd
         perror("ERROR: pipe() failed");
         exit(1);
     }
-
     fprintf(stderr, "(parent_process>forking...)\n");
-
-    if ((child1_pid = fork()) == -1) { // Fork the first child process
+    if ((c1pid = fork()) == -1) { // Fork the first child process
         perror("ERROR: fork() failed for child1");
         exit(1);
     }
-
-    if (child1_pid == 0) { // Child1 process
+    if (c1pid > 0) {
+        fprintf(stderr, "(parent_process>created process with id: %d)\n", c1pid);
+    }
+    if (c1pid == 0) { // Child1 process
         fprintf(stderr, "(child1>redirecting stdout to the write end of the pipe...)\n");
         close(STDOUT_FILENO); // Close standard output
-        dup(pipe_fd[1]); // Duplicate the write end of the pipe to standard output
-        close(pipe_fd[1]); // Close the original write end of the pipe
-        close(pipe_fd[0]); // Close the read end of the pipe
-
+        dup(pipeFd[1]); // Duplicate the write end of the pipe to standard output
+        close(pipeFd[1]); // Close the original write end of the pipe
+        close(pipeFd[0]); // Close the read end of the pipe
         fprintf(stderr, "(child1>going to execute cmd: ls -l)\n");
         execlp("ls", "ls", "-l", NULL); // Execute "ls -l"
         perror("ERROR: execlp() failed for ls -l"); // If execlp returns, an error occurred
         exit(1);
     }
-
-    // Parent process
-    close(pipe_fd[1]); // Close the write end of the pipe
+    close(pipeFd[1]); // Close the write end of the pipe
     fprintf(stderr, "(parent_process>closing the write end of the pipe...)\n");
-
     fprintf(stderr, "(parent_process>forking...)\n");
-
-    if ((child2_pid = fork()) == -1) { // Fork the second child process
+    if ((c2pid = fork()) == -1) { // Fork the second child process
         perror("ERROR: fork() failed for child2");
         exit(1);
     }
-
-    if (child2_pid == 0) { // Child2 process
+    if (c2pid == 0) { // Child2 process
         fprintf(stderr, "(child2>redirecting stdin to the read end of the pipe...)\n");
         close(STDIN_FILENO); // Close standard input
-        dup(pipe_fd[0]); // Duplicate the read end of the pipe to standard input
-        close(pipe_fd[0]); // Close the original read end of the pipe
-
+        dup(pipeFd[0]); // Duplicate the read end of the pipe to standard input
+        close(pipeFd[0]); // Close the original read end of the pipe
         fprintf(stderr, "(child2>going to execute cmd: tail -n 2)\n");
         execlp("tail", "tail", "-n", "2", NULL); // Execute "tail -n 2"
         perror("ERROR: execlp() failed for tail -n 2"); // If execlp returns, an error occurred
         exit(1);
     }
-
-    // Parent process
-    close(pipe_fd[0]); // Close the read end of the pipe
+    close(pipeFd[0]); // Close the read end of the pipe
     fprintf(stderr, "(parent_process>closing the read end of the pipe...)\n");
-
     fprintf(stderr, "(parent_process>waiting for child processes to terminate...)\n");
-
-    // Wait for both child processes to terminate
-    waitpid(child1_pid, NULL, 0);
-    waitpid(child2_pid, NULL, 0);
-
+    waitpid(c1pid, NULL, 0);
+    waitpid(c2pid, NULL, 0);
     fprintf(stderr, "(parent_process>exiting...)\n");
-
     return 0;
 }
--- Lab_C/myshell.c
+++ Lab_C/myshell.c
@@ -6,132 +6,198 @@
 #include <sys/wait.h>
 #include <limits.h>
 #include "LineParser.h"
-#include <asm-generic/fcntl.h>
+#include <fcntl.h>
 #include <linux/limits.h>
-
-
 
 int debug = 0;
 
-void execute(cmdLine *pCmdLine) {
+void executeCommand(cmdLine *pCmdLine) {
+    // Handle input redirection
+    if (pCmdLine->inputRedirect != NULL) {
+        int input_fd = open(pCmdLine->inputRedirect, O_RDONLY);
+        if (input_fd == -1) {
+            perror("ERROR: open() failed for input redirection");
+            exit(1);
+        }
+        if (dup2(input_fd, STDIN_FILENO) == -1) {
+            perror("ERROR: dup2() failed for input redirection");
+            exit(1);
+        }
+        close(input_fd);
+    }
+    
+    // Handle output redirection
+    if (pCmdLine->outputRedirect != NULL) {
+        int output_fd = open(pCmdLine->outputRedirect, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        if (output_fd == -1) {
+            perror("ERROR: open() failed for output redirection");
+            exit(1);
+        }
+        if (dup2(output_fd, STDOUT_FILENO) == -1) {
+            perror("ERROR: dup2() failed for output redirection");
+            exit(1);
+        }
+        close(output_fd);
+    }
+    
+    // Execute the command
+    if (execvp(pCmdLine->arguments[0], pCmdLine->arguments) == -1) {
+        perror("ERROR: execvp() failed");
+        exit(1);
+    }
+}
 
-    if (strcmp((*pCmdLine).arguments[0], "cd") == 0) { 
-        if (chdir((*pCmdLine).arguments[1]) == -1) {  //arguments[1] is the path to the directory
-            fprintf(stderr, "cd: No such file or directory\n", (*pCmdLine).arguments[1]); 
+void execute(cmdLine *pCmdLine) {
+    if (strcmp(pCmdLine->arguments[0], "cd") == 0) { 
+        if (chdir(pCmdLine->arguments[1]) == -1) {  // arguments[1] is the path to the directory
+            fprintf(stderr, "cd: No such file or directory\n");
         }
     }
-    //Arguments[0] is the command name
-     else if (strcmp((*pCmdLine).arguments[0], "alarm") == 0) { // Check if the command is "alarm"
-        pid_t pid = atoi((*pCmdLine).arguments[1]); 
-        if (kill(pid, SIGCONT) == 0) { // Send SIGCONT signal to wake up the process
-            printf("Process %d woke up\n", pid);
-        } else {
-            perror("ERROR: kill()"); 
+    // Arguments[0] is the command name
+    else if (strcmp(pCmdLine->arguments[0], "alarm") == 0) { // Check if the command is "alarm"
+        if(pCmdLine->arguments[1] == NULL) {
+            fprintf(stderr, "ERROR: No PID provided\n");
         }
-    } else if (strcmp((*pCmdLine).arguments[0], "blast") == 0) { // Check if the command is "blast"
-        pid_t pid = atoi((*pCmdLine).arguments[1]); 
-        if (kill(pid, SIGKILL) == 0) { // Send SIGKILL signal to terminate the process
-            printf("Process %d terminated\n", pid);
-        } else {
-            perror("ERROR: kill() failed\n"); 
-        } 
+        else {
+            pid_t pid = atoi(pCmdLine->arguments[1]); 
+            if (kill(pid, SIGCONT) == 0) { // Send SIGCONT signal to wake up the process
+                printf("Process %d woke up\n", pid);
+            } else {
+                perror("ERROR: kill()"); 
+            }
+        }
+    } else if (strcmp(pCmdLine->arguments[0], "blast") == 0) { // Check if the command is "blast"
+        if(pCmdLine->arguments[1] == NULL) {
+            fprintf(stderr, "ERROR: No PID provided\n");
+        }
+        else {
+            pid_t pid = atoi(pCmdLine->arguments[1]); 
+            if (kill(pid, SIGKILL) == 0) { // Send SIGKILL signal to terminate the process
+                printf("Process %d terminated\n", pid);
+            } else {
+                perror("ERROR: kill() failed\n"); 
+            } 
+        }
     }
-    else {
+    else if (pCmdLine->next != NULL) { // Check if the command is a pipe 
+        int pipeFd[2]; // Array to hold the read and write file descriptors for the pipe - pipeFd[0] read, pipeFd[1] write
+        pid_t c1pid, c2pid; // Process IDs for the child processes
+        if (pipe(pipeFd) == -1) { // Create the pipe and store the file descriptors in pipeFd
+            perror("ERROR: pipe() failed");
+            exit(1);
+        }
+        
+        // Check for invalid redirections
+        if (pCmdLine->outputRedirect != NULL) {
+            fprintf(stderr, "ERROR: Output redirection for the first command is invalid\n");
+            return;
+        }
+        if (pCmdLine->next->inputRedirect != NULL) {
+            fprintf(stderr, "ERROR: Input redirection for the second command is invalid\n");
+            return;
+        }
+        fprintf(stderr, "(parent_process>forking...)\n");
+        
+        // Fork the first child process
+        if ((c1pid = fork()) == -1) { 
+            perror("ERROR: fork() failed for child1");
+            exit(1);
+        }
+        if (c1pid == 0) { // Child1 process
+            fprintf(stderr, "(child1>redirecting stdout to the write end of the pipe...)\n");
+            close(STDOUT_FILENO); // Close standard output
+            if (dup2(pipeFd[1], STDOUT_FILENO) == -1) { // Duplicate the write end of the pipe to standard output
+                perror("ERROR: dup2() failed for child1");
+                exit(1);
+            }
+            close(pipeFd[1]); // Close the original write end of the pipe
+            close(pipeFd[0]); // Close the read end of the pipe
+            fprintf(stderr, "(child1>going to execute the first cmd)\n");
+            executeCommand(pCmdLine);
+            exit(1);
+        }
+        
+        // Parent process
+        close(pipeFd[1]); // Close the write end of the pipe
+        fprintf(stderr, "(parent_process>closing the write end of the pipe...)\n");
+        fprintf(stderr, "(parent_process>forking...)\n");
+
+        // Fork the second child process
+        if ((c2pid = fork()) == -1) { 
+            perror("ERROR: fork() failed for child2");
+            exit(1);
+        }
+        if (c2pid == 0) { // Child2 process
+            fprintf(stderr, "(child2>redirecting stdin to the read end of the pipe...)\n");
+            close(STDIN_FILENO); // Close standard input
+            if (dup2(pipeFd[0], STDIN_FILENO) == -1) { // Duplicate the read end of the pipe to standard input
+                perror("ERROR: dup2() failed for child2");
+                exit(1);
+            }
+            close(pipeFd[0]); // Close the original read end of the pipe
+            fprintf(stderr, "(child2>going to execute cmd: )\n");
+            executeCommand(pCmdLine->next);
+            exit(1);
+        }
+        
+        // Parent process
+        close(pipeFd[0]); // Close the read end of the pipe
+        fprintf(stderr, "(parent_process>closing the read end of the pipe...)\n");
+        fprintf(stderr, "(parent_process>waiting for child processes to terminate...)\n");
+        waitpid(c1pid, NULL, 0);
+        waitpid(c2pid, NULL, 0);
+        fprintf(stderr, "(parent_process>exiting...)\n");
+    } else {
         pid_t p = fork();
-        if(p ==-1){
+        if (p == -1) {
             perror("ERROR: fork() failed\n");
             exit(1);
         }
         if (p == 0) {
-            if((*pCmdLine).inputRedirect != NULL){
-                int input_file = open((*pCmdLine).inputRedirect, O_RDONLY);
-            if (input_file == -1) {
-                perror("ERROR: couldn't open the input file for reading");
-                _exit(1);
-            }
-            // Duplicate the file descriptor to stdin (file descriptor 0)
-            int result = dup2(input_file, STDIN_FILENO);
-            if (result == -1) {
-                perror("ERROR: couldn't accomplish dup2() for input redirection");
-                _exit(1);
-            }
-            close(input_file);
-            }
-            if((*pCmdLine).outputRedirect != NULL){
-                int output_file = open((*pCmdLine).outputRedirect, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-                if (output_file == -1) {
-                    perror("ERROR: couldn't open the output file for writing");
-                    _exit(1);
-                }
-                // Duplicate the file descriptor to stdout (file descriptor 1)
-                int result = dup2(output_file, STDOUT_FILENO);
-                if (result == -1) {
-                    perror("ERROR: couldn't accomplish dup2() for output redirection");
-                    _exit(1);
-                }
-                result = dup2(output_file, STDERR_FILENO); // Redirect stderr to the same file descriptor as stdout
-                if (result == -1) {
-                    perror("ERROR: couldn't accomplish dup2() for error redirection");
-                    _exit(1);
-                }
-                close(output_file);
-            }
-            if(debug){
-                fprintf(stderr, "PID: %d\n", getpid());
-                fprintf(stderr, "Executing command: %s\n", (*pCmdLine).arguments[0]);
-            }
-            execvp((*pCmdLine).arguments[0], (*pCmdLine).arguments);
-            perror("ERROR: execvp() failed\n"); // If execvp returns, it means an error occurred, and the current process was not replaced. 
-            _exit(1); //Used in situations where the process needs to terminate immediately without 
-            //performing any cleanup, such as in the child process after a fork where performing such cleanup could be 
-            //problematic or redundant.
-        }
-        else{
+            executeCommand(pCmdLine);
+        } else {
             if ((*pCmdLine).blocking) { // if blocking, wait for the child process to finish
-                int status; //stores the exit status of the child process
+                int status; // stores the exit status of the child process
                 waitpid(p, &status, 0); // If blocking is false, the shell will not wait for the child process to finish.
             }
         }
     }
-
 }
 
-int main(int argc, char **argv){
+int main(int argc, char **argv) {
     char cwd[PATH_MAX]; 
     char input[2048]; 
     cmdLine *parsedLine;
-    for(int i=0; i<argc; i++){
-        if(strcmp(argv[i], "-d") == 0){
+    for (int i = 0; i < argc; i++) {
+        if (strcmp(argv[i], "-d") == 0) {
             debug = 1;
         }
     }
 
-    while(1){
-        if(getcwd(cwd, sizeof(cwd))!=NULL){ // get current working directory
+    while (1) {
+        if (getcwd(cwd, sizeof(cwd)) != NULL) { // get current working directory
             printf("%s\n", cwd); 
             fflush(stdout);
-        }
-        else{
+        } else {
             perror("ERROR: getcwd() failed\n"); 
             return 1; 
         }
-        if(fgets(input, 2048, stdin)!=NULL){ // read input from the user
+        if (fgets(input, 2048, stdin) != NULL) { // read input from the user
             size_t len = strlen(input);
             if (len > 0 && input[len - 1] == '\n') {
                 input[len - 1] = '\0'; // remove newline character
             }
             if (strcmp(input, "quit") == 0) {
-                    break;
+                break;
             }
             parsedLine = parseCmdLines(input);
-            if(parsedLine == NULL){
+            if (parsedLine == NULL) {
                 perror("ERROR: parseCmdLines() failed\n");
                 continue;
             }
             execute(parsedLine);
             freeCmdLines(parsedLine);
-        }
-        else{
+        } else {
             perror("ERROR: fgets() failed\n");
             printf("\n");
             return 1; 
@@ -139,3 +205,5 @@ int main(int argc, char **argv){
     }
     return 0;
 }
+
+
